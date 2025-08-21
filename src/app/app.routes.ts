40fc10dbe17a0179a973344b0bed4fdb29d1910b
isaMirka <quispesicham@gmail.com>
2025-08-20 20:19:59 -0500

    implemetación del login finalizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,20 @@
 import { Routes } from '@angular/router';
 import {PortalLayout} from './layouts/components/portal-layout/portal-layout';
 import {PortalHome} from './features/transacciones/pages/portal-home/portal-home';
+import {AuthGuard} from './core/auth/guards/auth.guard';
+import { Login } from './features/authentication/componentes/login/login';
 
 export const routes: Routes = [
   {
     path: "portal",
     component: PortalLayout,
+    canActivate: [AuthGuard],
     children: [
       {
         path: '',
         component: PortalHome
       }
     ]
-  }
+  },
+  {path: 'login', component: Login}
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
+import 'zone.js';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app/app.routes';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient()   // 👈 agregado aquí
+  ]
+};
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
